82c8320e846a6c1590206ae96a49d9ef99f9b6d3
MateusOrlandinDias <mateus.orlandin@gmail.com>
2023-06-03 21:22:58 -0300

    Feature\ Login done

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,16 +1,39 @@
 import './styles.css';
+import logo from '../../assets/Meu-time-logo.png'
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import api from '../../services/api';
+const axios = require('axios');
 
-export default function Login(){
+export default function Login() {
     const [apiKey, setApiKey] = useState('');
-    
+
     const navigate = useNavigate();
 
-    return(
+    async function LoginApiFootball(event) {
+        event.preventDefault();
+
+        try {
+            const response = await api.get('countries');
+            localStorage.setItem('apiKey', apiKey);
+        } catch (error) {
+            alert('Erro ao autenticar API Key: ' + error);
+        }
+    }
+
+    return (
         <div className='login-container'>
-            
+            <img alt='Logo' src={logo} />
+            <form onSubmit={LoginApiFootball}>
+                <h1>Token de acesso API-Football</h1>
+                <input
+                    placeholder='API Key'
+                    value={apiKey}
+                    onChange={e => setApiKey(e.target.value)}
+                />
+                <button className='button' type="submit">Login</button>
+            </form>
         </div>
     );
-    
+
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://localhost:44369",
+    baseURL: "https://api-football-v1.p.rapidapi.com/v3",
+    headers: {
+        'X-RapidAPI-Key': 'dc4e64af3amsh202547aba551593p1007bdjsn8b17b8a84c67',
+        'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+    },
 });
 
 export default api;
